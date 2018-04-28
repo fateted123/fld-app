6498fd2b2307a4f6a64533c36450cdd16ae1fcce
wangxuetao <xuetaowang@xuetaowangdeMacBook-Pro.local>
2018-04-28 22:32:09 +0800

    增加IP工具类和日志文件

---
+++ src/main/java/com/xz/fld/util/IPUtils.java
@@ -0,0 +1,52 @@
+package com.xz.fld.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IPUtils {
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Real-IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-forwarded-for");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        //防止多级代理时返回过个ip。
+        if(ip != null && ip.indexOf(",") != -1){
+            ip= ip.substring(0,ip.indexOf(","));
+        }
+
+        return ip;
+    }
+
+    public static String getAllIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Real-IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-forwarded-for");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
+
+}
